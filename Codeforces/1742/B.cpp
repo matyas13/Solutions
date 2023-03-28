6f1ab53225c9e9244c7f9747646960110d419277
Matyas <krizbai.matyas1@gmail.com>
2023-03-28 20:39:41 +0300

    practice

---
+++ Codeforces/1742/B.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve(){
+	int n;
+	cin >> n;
+	vector <int> v(n);
+	for(int i=0; i<n; i++){
+		cin >> v[i];
+	}	
+	sort(v.begin(), v.end());
+	for(int i=1; i<n; i++){
+		if(v[i]==v[i-1]){
+			cout << "NO\n";
+			return;
+		}
+	}
+	cout << "YES\n";
+}
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
