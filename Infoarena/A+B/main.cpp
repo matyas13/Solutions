7303b2bea898c0ad90ead8f17d1713fc4cb43aa4
Matyas <krizbai.matyas1@gmail.com>
2023-04-30 21:27:08 +0300

    practice

---
+++ Infoarena/A+B/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream fin("adunare.in");
+ofstream fout("adunare.out");
+
+int main(){
+   int a, b;
+   fin >> a >> b;
+   fout << a+b;
+   fin.close();
+   fout.close();
+   return 0;
+}
